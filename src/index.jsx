50976343b98750a6428084e1e839fd968dbeb4a7
Brian Lauber <constructible.truth@gmail.com>
2015-11-12 12:55:36 -0500

    Added Babel 6

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,12 @@
 import React from "react";
 
 export default function factory(factory_kwargs) {
-  
+
 
   return function(kwargs) {
     return (
       <input
-        type="radio"
-        name={name} />
+        type="radio" />
     )
   }
 }
