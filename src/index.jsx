f07af048444fb20390590f10a20817f35a22df96
Brian Lauber <constructible.truth@gmail.com>
2015-11-12 14:35:51 -0500

    Basic factory implementation
    I'll need to go back and cleanup the code.  But, the basic
    factory method appears to be working.

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,29 @@
 import React from "react";
 
 export default function factory(factory_kwargs) {
+  const factory_props = {
+    name: factory_kwargs.name
+  };
 
+  if(factory_kwargs.hasOwnProperty("onChange")) {
+    factory_props.onChange = factory_kwargs.onChange;
+  }
 
-  return function(kwargs) {
+  return function(instance_props) {
+    const creation_props = {};
+
+    // If this radio button's value matches the selectedValue,
+    // then render it as 'checked'.
+    if(factory_kwargs.hasOwnProperty("selectedValue")) {
+      creation_props.checked = (instance_props.value === factory_kwargs.selectedValue);
+    }
+
     return (
       <input
-        type="radio" />
+        type="radio"
+        {...factory_props}
+        {...creation_props}
+        {...instance_props} />
     )
   }
 }
