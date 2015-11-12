c8975158019fdae266c4425489b6d83ebac34a2a
Brian Lauber <constructible.truth@gmail.com>
2015-11-12 12:06:12 -0500

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+export default function factory(factory_kwargs) {
+  
+
+  return function(kwargs) {
+    return (
+      <input
+        type="radio"
+        name={name} />
+    )
+  }
+}
